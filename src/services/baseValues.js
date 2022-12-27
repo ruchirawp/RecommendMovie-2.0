1eadf31c151d9b9dcce95505aec0edabb6313dde
ruchi <ruchirawperera@gmail.com>
2022-12-27 00:45:02 -0500

    added new proxy

--- src/services/baseValues.js
+++ src/services/baseValues.js
@@ -1,5 +1,6 @@
 
-export const baseUrl = "http://localhost:3001";
+// export const baseUrl = "http://localhost:3001";
+export const baseUrl = "https://recommendmovie.onrender.com/";
 
 export default {baseUrl}
 
