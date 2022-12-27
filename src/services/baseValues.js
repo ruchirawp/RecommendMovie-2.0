3445e5323ff3399c835a890b90c3d474cb36acf9
ruchi <ruchirawperera@gmail.com>
2022-12-27 00:52:54 -0500

    fixed path

--- src/services/baseValues.js
+++ src/services/baseValues.js
@@ -1,6 +1,6 @@
 
 // export const baseUrl = "http://localhost:3001";
-export const baseUrl = "https://recommendmovie.onrender.com/";
+export const baseUrl = "https://recommendmovie-api.onrender.com";
 
 export default {baseUrl}
 
